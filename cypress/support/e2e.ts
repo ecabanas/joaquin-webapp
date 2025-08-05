a202cad85e9e0bd6dce4382e8f2d17fa6e9ed314
Eric Cabañas <ecalabcn@gmail.com>
2025-08-05 11:00:39 +0000

    Now I got the following error. start server command "npm start" current

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,13 @@
+// This is a support file for Cypress.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+//
+// You can change the location of this file or turn off
+// support file processing with the 'supportFile' configuration option.
+
+// Import commands.js using ES2015 syntax:
+// import './commands'
+
+// Alternatively, you can use CommonJS syntax:
+// require('./commands')
